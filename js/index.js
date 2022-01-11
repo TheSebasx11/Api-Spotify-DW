a7233a185caba842267eb5ee7fd4b3ee9386699c
TheSebasx11 <sebabalcar18@gmail.com>
2022-01-10 21:57:51 -0500

    Beta v1

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ const ApiControl = (function () {
       }
     );
     const data = await result.json();
-    return data;
+    return data.tracks;
   };
 
   return {
@@ -76,7 +76,7 @@ const ApiControl = (function () {
   };
 })();
 
-mostrarCosita = async () => {
+Mostrar = async () => {
   const token = await ApiControl.getToken();
   //const nombre = "Pink Floyd";
   const tf = document.getElementById("search_input");
@@ -85,7 +85,7 @@ mostrarCosita = async () => {
   const artistID = artista.id;
   const albumes = await ApiControl.getAlbumsFromArtist(token, artistID);
   const topSongs = await ApiControl.getTopSongsFromArtist(token, artistID);
-  console.log(albumes);
+  console.log(topSongs);
 };
 
 Mostrar_albumes = async () => {
@@ -93,7 +93,7 @@ Mostrar_albumes = async () => {
   const tf = document.getElementById("search_input");
   const element = document.createElement("div");
   let nombre = tf.value;
-  const div = document.getElementById("table_container");
+  const divApp = document.getElementById("table_container");
   const artist = await ApiControl.getArtist(token, nombre);
   const id = artist.id;
   const albumes = await ApiControl.getAlbumsFromArtist(token, id);
@@ -101,12 +101,12 @@ Mostrar_albumes = async () => {
   let htmlTabla = `
         <table id="tabla" class="w-full border-collapse border">
             <thead>
-                <tr class="bg-grey-100 border">
-                    <th class="text-left text-white">Numero</th>
-                    <th class="text-left text-white">Titulo</th>
-                    <th class="text-left text-white">Fecha de publicacion</th>
-                    <th class="text-left text-white">Total de canciones</th>
-                    <th class="text-left text-white">Escuchar</th>
+                <tr class="border">
+                    <th class="text-left text-white border p-2 bg-[#024532]">Numero</th>
+                    <th class="text-left text-white border p-2 bg-[#024532]">Titulo</th>
+                    <th class="text-left text-white border p-2 bg-[#024532]">Fecha de publicacion</th>
+                    <th class="text-left text-white border p-2 bg-[#024532]">Total de canciones</th>
+                    <th class="text-left text-white border p-2 bg-[#024532]">Escuchar</th>
                 </tr>
             </thead>
             <tbody>    
@@ -114,18 +114,92 @@ Mostrar_albumes = async () => {
   albumes.forEach((element, index) => {
     htmlTabla += `
         <tr>
-            <td class="text-white">${index + 1}</td>
-            <td class="text-white">${element.name}</td>
-            <td class="text-white">${element.release_date}</td>
-            <td class="text-white">${element.total_tracks}</td>
-            <td class="text-white"><a href="${
+            <td class="text-white border p-2 ">${index + 1}</td>
+            <td class="text-white border p-2">${element.name}</td>
+            <td class="text-white border p-2">${element.release_date}</td>
+            <td class="text-white border p-2">${element.total_tracks}</td>
+            <td class="text-white border p-2"><a href="${
               element.external_urls.spotify
-            }" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(6, 49, 24, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m9 17 8-5-8-5z"></path></svg></a></td>
+            }" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #000 transform: ;msFilter:;" class="hover:scale-125 duration-150 hover:fill-white"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m9 17 8-5-8-5z"></path>
+        </svg>
+        </a>
+        </td>
         </tr>
         `;
   });
   htmlTabla += `</tbody></table>`;
   element.innerHTML = htmlTabla;
-  div.appendChild(element);
-  
+  if (divApp.childElementCount >= 1) {
+    divApp.innerHTML = "";
+  }
+  divApp.appendChild(element);
+};
+
+function milistomins(ms) {
+  (min = Math.floor((ms / 1000 / 60) << 0)),
+    (sec = Math.floor((ms / 1000) % 60));
+  minS = min < 10 ? `0${min}` : `${min}`;
+  secS = sec < 10 ? `${sec}0` : `${sec}`;
+  return `${minS}:${secS}`;
+}
+
+Mostrar_top = async () => {
+  const token = await ApiControl.getToken();
+  const tf = document.getElementById("search_input");
+  const element = document.createElement("div");
+  let nombre = tf.value;
+  const divApp = document.getElementById("table_container");
+  const artist = await ApiControl.getArtist(token, nombre);
+  const id = artist.id;
+  const topSongs = await ApiControl.getTopSongsFromArtist(token, id);
+
+  let htmlTabla = `
+  <table id="tabla2" class="w-full border-collapse border">
+        <thead>
+            <tr class="bg-grey-100 border">
+                <th class="text-left text-white border p-2 bg-[#024532]">Numero</th>
+                <th class="text-left text-white border p-2 bg-[#024532]">Titulo</th>
+                <th class="text-left text-white border p-2 bg-[#024532]">Duracion</th>
+                <th class="text-left text-white border p-2 bg-[#024532]">Album</th>
+                <th class="text-left text-white border p-2 bg-[#024532]">Colaborador</th>
+                <th class="text-left text-white border p-2 bg-[#024532]">Escuchar</th>
+            </tr>
+        </thead>
+    <tbody>  
+  `;
+  colab = ``;
+  topSongs.forEach((element, index) => {
+    element.artists.forEach((element2, index) => {
+      if (index != element.artists.length - 1) {
+        colab += `${element2.name}, `;
+      }else{
+        colab += `${element2.name}`;
+      }
+    });
+    htmlTabla += `
+    <tr class="h-15 ">
+        <td class="text-white border p-2">${index + 1}</td>
+        <td class="text-white border p-2">${element.name}</td>
+        <td class="text-white border p-2">${milistomins(
+          element.duration_ms
+        )}</td>
+        <td class="text-white border p-2">${element.album.name}</td>
+        <td class="text-white border p-2">${colab}</td>
+        <td class="text-white border p-2">
+            <a href="${element.external_urls.spotify}" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #000 transform: ;msFilter:;" class="hover:scale-125 duration-150 hover:fill-white"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m9 17 8-5-8-5z"></path>
+            </svg>
+            </a>
+        </td>
+    </tr>
+`;
+    colab = ``;
+  });
+  htmlTabla += `</tbody></table>`;
+  element.innerHTML = htmlTabla;
+  if (divApp.childElementCount >= 1) {
+    divApp.innerHTML = "";
+  }
+  divApp.appendChild(element);
 };
